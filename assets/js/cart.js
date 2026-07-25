b17bae09ea208aea5b572d6ec6a8423c81c178b1
brhcareerinfo-collab <brhcareer.info@gmail.com>
2026-07-26 04:34:15 +0530

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,5 +1,29 @@
-document.getElementById("cart-items").innerHTML = `
-<h2 style="text-align:center;">
-🛒 Cart Page Coming Soon
-</h2>
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const cartItems = document.getElementById("cart-items");
+
+if(cart.length==0){
+
+cartItems.innerHTML="<h2 style='text-align:center'>🛒 Cart Empty</h2>";
+
+}else{
+
+cart.forEach(item=>{
+
+cartItems.innerHTML +=`
+
+<div class="product-card">
+
+<img src="${item.image}">
+
+<h3>${item.name}</h3>
+
+<p>${item.price}</p>
+
+</div>
+
 `;
+
+});
+
+}
