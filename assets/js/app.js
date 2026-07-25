98c000cc9a350140f08eca8ade2530f07a565a5e
brhcareerinfo-collab <brhcareer.info@gmail.com>
2026-07-26 01:58:02 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
+// ===== GaonFresh App =====
+let cart = 0;
 
+const buttons = document.querySelectorAll(".product-card button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        cart++;
+
+        document.getElementById("cart-count").innerText = cart;
+
+        alert("✅ Product Added to Cart");
+    });
+});
+
+console.log("GaonFresh Started Successfully");
